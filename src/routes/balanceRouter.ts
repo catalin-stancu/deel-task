1dceb335f1b5f29eb2dac692d2bd1e3d44578b49
Catalin Stancu <catalin.stancu@xoomworks.com>
2023-05-11 10:41:41 +0300

    Update readme; Add postman collection

--- src/routes/balanceRouter.ts
+++ src/routes/balanceRouter.ts
@@ -10,6 +10,8 @@ const router = Router({ caseSensitive: false });
 // Not very clear if the logged in user is the one who deposits money into another user's balance.
 // Is he transfering money from his own balance, or some external money?
 // For simplicity I'm going to assume we're talking about the second case.
+// In order for this API to accept the deposit, the version provided in the If-Match header
+// Must be the exact current version of the profile you are trying to update
 router.post('/balances/deposit/:userId',
   getProfile,
   validate(balanceDepositSchema),
